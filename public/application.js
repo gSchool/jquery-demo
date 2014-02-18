cfb3cdea03150d215a0fd8c904de812fac48b365
Mike Gehard <mike.gehard@gmail.com>
2014-02-18 14:53:17 -0700

    Get jQuery wired up.

--- public/application.js
+++ public/application.js
@@ -1,1 +1,11 @@
 /* Put all javascript here. */
+
+/* We must wait until the document is ready to be manipulated
+    See: http://learn.jquery.com/using-jquery-core/document-ready/
+ */
+
+$(function() {
+    alert("Look at the logs in the console for a message");
+    console.log( "ready!" );
+});
+
